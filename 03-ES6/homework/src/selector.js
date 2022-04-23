db181ee41d5a74815767e46dfd3bee8ab51fa929
Eliz <longartcoll@gmail.com>
2022-04-23 10:02:35 -0500

    es6 incompleto

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,7 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +18,7 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  return selector[0] === '#' ? 'id' : selector[0] === '.' ? 'class' : selector.includes('.') ? 'tag.class' : 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +30,45 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
+/*     matchFunction = function(elementoDelDom){
+      if('#' + elementoDelDom.id === selector){
+        return true
+      } else return false;
+    } */
+    matchFunction = (elementoDelDom) => `#${elementoDelDom.id}` === selector
    
   } else if (selectorType === "class") {
+/*     matchFunction = function(elementoDelDom){
+      let arrayClasesIncluidas = elementoDelDom.classList;
+      for (let i = 0; i < arrayClasesIncluidas.length; i++) {
+        if('.' + arrayClasesIncluidas[i] === selector) return true        
+      }
+      return false  */
+
+    matchFunction = (elementoDelDom) => elementoDelDom.classList.contains(selector.substring(1)) //el substring arranca la palabra desde la posición que yo le pase por argumento
+
     
   } else if (selectorType === "tag.class") {
-    
+    /*matchFunction = function(elementoDelDom){ 
+       let tagClass = selector.split('.');
+      let tagBuscado = tagClass[0];
+      let classBuscado = tagClass[1]; 
+      let [tagBuscado, classBuscado] = selector.split('.'); // va a devolverme el tagClass en la posicion 0 y 1 para cada valor respectivamente.
+      
+      let matchFunctionTag = matchFunctionMaker(tagBuscado);
+      let respuestaMatchTag = matchFunctionTag(elementoDelDom);
+      
+      let matchFunctionClass = matchFunctionMaker('.' + classBuscado);
+      let respuestaMatchClass = matchFunctionClass(elementoDelDom);
+
+       return respuestaMatchClass && respuestaMatchTag;
+    */
+
+    matchFunction = (elementoDelDom) => matchFunctionMaker(selector.split('.')[0])(elementoDelDom) && matchFunctionMaker(`.${selector.split('.')[1]}`)(elementoDelDom)
+
+
   } else if (selectorType === "tag") {
-    
+    matchFunction = (elementoDelDom) => elementoDelDom.tagName.toLowerCase() === selector.toLowerCase() //lo paso a minuscula porque la consola me lo devuelve en mayuscula siempre
   }
   return matchFunction;
 };
